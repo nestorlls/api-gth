eac63b9937be32f7d4c52ca33ccbc3dc04464cc4
Paul Llanque <llanquepol@gmail.com>
2024-01-03 13:56:10 -0500

    [ADD] config use cases

---
+++ src/domain/use-cases/use-cases.ts
@@ -0,0 +1,34 @@
+import { Login, Register } from './auth';
+import { GetUserProperties } from './userProperty/getAll';
+import { DeleteUser, GetUserById, GetallUsers, UpdateUser } from './user';
+import { AddFavorite, GetAllFavorites, RemoveFavorite, UpdateFavorite } from './userFavorites';
+import { CreateProperty, DeleteProperty, GetAllProperties, GetPropertyById, UpdateProperty } from './properties';
+
+export const useCases = {
+  auth: {
+    Login,
+    Register,
+  },
+  users: {
+    GetallUsers,
+    GetUserById,
+    UpdateUser,
+    DeleteUser,
+  },
+  properties: {
+    GetAllProperties,
+    GetPropertyById,
+    CreateProperty,
+    UpdateProperty,
+    DeleteProperty,
+  },
+  userfavorites: {
+    GetAllFavorites,
+    AddFavorite,
+    UpdateFavorite,
+    RemoveFavorite,
+  },
+  userProperties: {
+    GetUserProperties,
+  },
+};
