d187bacf4863d8ab3148d13c3ccc16bbae7d2c7e
Paul Llanque <llanquepol@gmail.com>
2024-01-04 11:18:02 -0500

    [ADD] user routes

---
+++ src/presentation/controllers/user/routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import { User } from '@data/mongo/models';
+import { UserDataSourceImpl } from '@infrastructure/datasource';
+import { UserRepositoryImpl } from '@infrastructure/repository';
+import { AuthMiddleware } from '@presentation/middlewares';
+import { UserServices } from '@presentation/service';
+import { UserController } from './controller';
+
+export class UserRoutes {
+  static get routes(): Router {
+    const router = Router();
+
+    const datasource = new UserDataSourceImpl(User);
+    const repository = new UserRepositoryImpl(datasource);
+    const service = new UserServices(repository);
+    const controller = new UserController(service);
+
+    router.use(AuthMiddleware.authenticate);
+
+    router.get('/', controller.getAllUsers);
+    router.get('/:id', controller.getUserById);
+    router.patch('/:id', controller.updateUser);
+    router.delete('/:id', controller.deleteUser);
+
+    return router;
+  }
+}
