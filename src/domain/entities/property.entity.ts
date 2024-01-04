a623bab17c957537a885b5730485b0343695febc
Paul Llanque <llanquepol@gmail.com>
2024-01-04 13:12:57 -0500

    [UPDATE] property entity with clearly message error

--- src/domain/entities/property.entity.ts
+++ src/domain/entities/property.entity.ts
@@ -37,23 +37,22 @@ export class PropertyEntity {
   }
 
   static fromObject(props: { [key: string]: any }): PropertyEntity {
-    if (!props.id) throw CustomeError.badRequest('Missing property id');
-    if (!Validator.isMongoId(props.id)) throw CustomeError.badRequest('Invalid property id');
-    if (!props.type) throw CustomeError.badRequest('Missing type');
-    if (!props.address) throw CustomeError.badRequest('Missing property address');
-    if (props.rent === undefined) throw CustomeError.badRequest('Missing rent price');
-    if (props.maintance === undefined) throw CustomeError.badRequest('Missing maintance price');
-    if (props.price === undefined) throw CustomeError.badRequest('Missing price sale');
-    if (!props.propertyType) throw CustomeError.badRequest('Missing propertyType');
-    if (props.bedRooms === undefined) throw CustomeError.badRequest('Missing bedRooms');
-    if (props.bathRooms === undefined) throw CustomeError.badRequest('Missing bathRooms');
-    if (props.area === undefined) throw CustomeError.badRequest('Missing area');
-    if (props.petAllowed === undefined) throw CustomeError.badRequest('Missing petAllowed');
-    if (!props.description) throw CustomeError.badRequest('Missing description');
-    if (!props.images) throw CustomeError.badRequest('Missing images');
-    if (!props.status) throw CustomeError.badRequest('Missing status');
-    if (!props.user) throw CustomeError.badRequest('Missing userId');
-    if (!Validator.isMongoId(props.user)) throw CustomeError.badRequest('Invalid user id');
+    if (!props.id) throw CustomeError.badRequest('Missing property Id in Property Entity');
+    if (!Validator.isMongoId(props.id)) throw CustomeError.badRequest('Invalid property id Property Entity');
+    if (!props.type) throw CustomeError.badRequest('Missing type Property Entity');
+    if (!props.address) throw CustomeError.badRequest('Missing property address Property Entity');
+    if (props.rent === undefined) throw CustomeError.badRequest('Missing rent price Property Entity');
+    if (props.maintance === undefined) throw CustomeError.badRequest('Missing maintance price Property Entity');
+    if (props.price === undefined) throw CustomeError.badRequest('Missing price sale Property Entity');
+    if (!props.propertyType) throw CustomeError.badRequest('Missing propertyType Property Entity');
+    if (props.bedRooms === undefined) throw CustomeError.badRequest('Missing bedRooms Property Entity');
+    if (props.bathRooms === undefined) throw CustomeError.badRequest('Missing bathRooms Property Entity');
+    if (props.area === undefined) throw CustomeError.badRequest('Missing area Property Entity');
+    if (props.petAllowed === undefined) throw CustomeError.badRequest('Missing petAllowed Property Entity');
+    if (!props.description) throw CustomeError.badRequest('Missing description Property Entity');
+    if (!props.images) throw CustomeError.badRequest('Missing images Property Entity');
+    if (!props.status) throw CustomeError.badRequest('Missing status Property Entity');
+    if (!props.user) throw CustomeError.badRequest('Missing user Id Property Entity');
 
     return new PropertyEntity(props as PropertyEntity);
   }
