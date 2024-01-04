4ecc3a53dbb38a1eafad9f8aa7bda6f020ba148d
Paul Llanque <llanquepol@gmail.com>
2024-01-04 11:08:26 -0500

    [UPDATE] remove no-explicit-any

--- src/config/adapters/jwt.adapter.ts
+++ src/config/adapters/jwt.adapter.ts
@@ -4,11 +4,10 @@ import jwt from 'jsonwebtoken';
 const jwt_secret = envs.JWT_SECRET;
 
 export class JwtAdapter {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   static async generateToken(payload: string | object, duration: string = '1h'): Promise<string | null> {
     return new Promise((resolve) => {
       jwt.sign(payload, jwt_secret, { expiresIn: duration }, (err, token) => {
-        if (err) resolve(null);
+        if (err) return resolve(null);
         resolve(token as string);
       });
     });
@@ -17,7 +16,7 @@ export class JwtAdapter {
   static async verifytoken<T>(token: string): Promise<T | null> {
     return new Promise((resolve) => {
       jwt.verify(token, jwt_secret, (err, decored) => {
-        if (err) resolve(null);
+        if (err) return resolve(null);
         resolve(decored as T);
       });
     });
