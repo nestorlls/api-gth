ca090c0bc0beea58f78e7496ded3543c95ea5b06
Paul Llanque <llanquepol@gmail.com>
2024-01-04 11:10:33 -0500

    [ADD] user service

---
+++ src/presentation/service/user.service.ts
@@ -0,0 +1,25 @@
+import { UserRepository } from '@domain/abstracts/repository';
+import { UserService } from '@domain/abstracts/services';
+import { UpdateUserDto } from '@domain/dtos';
+import { PaginationDto, ReturnWithPaginateDto } from '@domain/dtos/shared';
+import { UserEntity } from '@domain/entities';
+
+export class UserServices implements UserService {
+  constructor(private readonly repository: UserRepository) {}
+
+  async getAllUsers(dto: PaginationDto): Promise<ReturnWithPaginateDto<UserEntity>> {
+    return await this.repository.getAllUsers(dto);
+  }
+
+  async getUserById(id: string): Promise<UserEntity> {
+    return await this.repository.getUserById(id);
+  }
+
+  async updateUser(dto: UpdateUserDto): Promise<UserEntity> {
+    return await this.repository.updateUser(dto);
+  }
+
+  async deleteUser(id: string): Promise<UserEntity> {
+    return await this.repository.deleteUser(id);
+  }
+}
