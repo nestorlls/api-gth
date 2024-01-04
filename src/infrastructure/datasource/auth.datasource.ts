74f90684d11d42700b0026896ac90a26c8b8539f
Paul Llanque <llanquepol@gmail.com>
2024-01-03 19:38:08 -0500

    [UPDATE] login and register methods with trycatch

--- src/infrastructure/datasource/auth.datasource.ts
+++ src/infrastructure/datasource/auth.datasource.ts
@@ -8,17 +8,32 @@ export class AuthDatasourceImpl implements AuthDatasource {
   constructor(private readonly model: typeof User) {}
 
   async login(dto: LoginDto): Promise<UserEntity> {
-    const { email } = dto;
-    const userExists = await this.model.findOne({ email });
-    if (!userExists) throw CustomeError.notFound('User not found');
-    return UserEntity.fromObject(userExists.toJSON());
+    let user;
+    try {
+      user = await this.model.findOne({ email: dto.email });
+    } catch (error) {
+      throw CustomeError.internalServerError(`${error}`);
+    }
+    if (!user) throw CustomeError.notFound('User not found');
+    return UserEntity.fromObject(user);
   }
 
   async register(dto: RegisterDto): Promise<UserEntity> {
-    const { email } = dto;
-    const userExists = await this.model.findOne({ email });
-    if (userExists) throw CustomeError.badRequest('User already exists');
-    const user = await this.model.create(dto);
+    let user;
+    try {
+      user = await this.model.findOne({ email: dto.email });
+    } catch (error) {
+      throw CustomeError.internalServerError(`${error}`);
+    }
+
+    if (user) throw CustomeError.badRequest('User already exists');
+
+    try {
+      user = await this.model.create(dto);
+    } catch (error) {
+      throw CustomeError.internalServerError(`${error}`);
+    }
+
     return UserEntity.fromObject(user.toJSON());
   }
 }
