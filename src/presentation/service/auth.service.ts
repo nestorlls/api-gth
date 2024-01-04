62feb14650ffe0e46e00e3e74ef66a6bd7760dd4
Paul Llanque <llanquepol@gmail.com>
2024-01-03 20:11:28 -0500

    [ADD] auth service

---
+++ src/presentation/service/auth.service.ts
@@ -0,0 +1,41 @@
+import { BcryptAdapter, JwtAdapter } from '@config/adapters';
+import { AuthRepository } from '@domain/abstracts/repository';
+import { AuthService, IUserAuth } from '@domain/abstracts/services/auth';
+import { LoginDto, RegisterDto } from '@domain/dtos';
+import { CustomeError } from '@domain/errors';
+
+export class AuthServices implements AuthService {
+  constructor(private readonly repository: AuthRepository) {}
+
+  async login(dto: LoginDto): Promise<IUserAuth> {
+    const { password, ...user } = await this.repository.login(dto);
+    let isMatch, token;
+    try {
+      isMatch = BcryptAdapter.compare(dto.password, password!);
+      token = await JwtAdapter.generateToken({ id: user.id, email: user.email }, '1h');
+    } catch (error) {
+      throw CustomeError.internalServerError(`${error}`);
+    }
+
+    if (!isMatch) throw CustomeError.badRequest('Invalid credentials');
+    if (!token) throw CustomeError.internalServerError('Error generating token');
+
+    return { user, token };
+  }
+
+  async register(dto: RegisterDto): Promise<IUserAuth> {
+    const passwordHashed = BcryptAdapter.hash(dto.password);
+    const user = await this.repository.register({ ...dto, password: passwordHashed });
+
+    let token;
+    try {
+      token = await JwtAdapter.generateToken({ id: user.id, email: user.email }, '1h');
+    } catch (error) {
+      throw CustomeError.internalServerError(`${error}`);
+    }
+
+    if (!token) throw CustomeError.internalServerError('Error generating token');
+
+    return { user, token };
+  }
+}
