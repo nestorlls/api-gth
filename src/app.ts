06b64fae1c3e658b51a127deb340b37747d30e40
Paul Llanque <llanquepol@gmail.com>
2024-01-03 19:43:00 -0500

    [UPATE] app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
+import { envs } from '@config/environments';
+import { MongoDB } from '@data/mongo/connection/mongo.connection';
+import { Routes } from '@presentation/rourtes';
+import { Server } from '@presentation/server';
+
 (async () => {
   await main();
 })();
 
 async function main() {
-  console.log('Hello, World!');
+  const { PORT, API_NAME, MONGO_DB_URL, MONGO_DB_NAME } = envs;
+  const mongoDB = new MongoDB();
+  const connected = await mongoDB.connection({ mongoUrl: MONGO_DB_URL, dbName: MONGO_DB_NAME });
+
+  const server = new Server({
+    port: PORT,
+    apiname: API_NAME,
+    routes: Routes.getRoutes(),
+  });
+
+  if (connected) {
+    console.log('MongoDB connected successfully!');
+    server.start();
+  }
 }
