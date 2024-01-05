fa8490d826b7a11ab45c07dc4318c6f3447d18a8
Paul Llanque <llanquepol@gmail.com>
2024-01-05 15:24:10 -0500

    [DELETE] favorite abstract datasource

--- src/domain/abstracts/datasource/favorite.ts
+++
@@ -1,8 +0,0 @@
-import { CreateFavoriteDto } from '@domain/dtos';
-import { FavoriteEntity } from '@domain/entities';
-
-export abstract class FavoriteDatasource {
-  abstract getUserFavorites(userId: string): Promise<FavoriteEntity[]>;
-  abstract addFavorite(dto: CreateFavoriteDto): Promise<FavoriteEntity>;
-  abstract removeFavorite(id: string): Promise<FavoriteEntity>;
-}
