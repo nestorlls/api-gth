7c436c4207d5c859a987b9f4553625f08e47d68c
Paul Llanque <llanquepol@gmail.com>
2024-01-04 20:35:38 -0500

    [REFACTOR] favorite entity

--- src/domain/entities/favorite.entity.ts
+++ src/domain/entities/favorite.entity.ts
@@ -1,28 +1,23 @@
-import { Validator } from '@config/helpers';
 import { CustomeError } from '@domain/errors';
 
 export class FavoriteEntity {
+  public readonly id: string;
   public readonly property: string;
   public readonly user: string;
-  public readonly contacted: boolean;
 
   private constructor(props: FavoriteEntity) {
-    const { property, user, contacted } = props;
+    const { id, property, user } = props;
+    this.id = id;
     this.user = user;
     this.property = property;
-    this.contacted = contacted;
   }
 
   static fromObject(props: { [key: string]: any }): FavoriteEntity {
-    const { property, user, contacted } = props;
+    const { id, property, user } = props;
 
     if (!property) throw CustomeError.badRequest('Missing property');
-    if (Validator.isMongoId(property)) throw CustomeError.badRequest('Invalid property id');
     if (!user) throw CustomeError.badRequest('Missing user');
-    if (!Validator.isMongoId(user)) throw CustomeError.badRequest('Invalid user id');
-    if (!contacted) throw CustomeError.badRequest('Missing contacted');
-    if (contacted !== true && contacted !== false) throw CustomeError.badRequest('Invalid contacted');
 
-    return new FavoriteEntity(props as FavoriteEntity);
+    return new FavoriteEntity({ id, property, user });
   }
 }
