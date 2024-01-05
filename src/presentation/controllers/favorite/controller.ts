ecd19816db6818efeaeea26fe18bc7370068632a
Paul Llanque <llanquepol@gmail.com>
2024-01-05 15:42:09 -0500

    [UPDATE] update names

--- src/presentation/controllers/favorite/controller.ts
+++ src/presentation/controllers/favorite/controller.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from 'express';
 
-import { FavoriteService } from '@domain/abstracts/services';
+import { FavoriteContactService } from '@domain/abstracts/services';
 import { useCases } from '@domain/use-cases/use-cases';
 import { HandleError } from '@presentation/errors/handle.error';
-import { CreateFavoriteDto } from '@domain/dtos';
+import { CreateFavoriteContactAdvDto } from '@domain/dtos';
 
 export class FavoriteController {
-  constructor(private readonly service: FavoriteService) {}
+  constructor(private readonly service: FavoriteContactService) {}
 
-  getUserFavorites = (req: Request, res: Response) => {
+  getFavoritesContactedProperties = (req: Request, res: Response) => {
     const { user } = req.body;
 
-    new useCases.userfavorites.GetAllFavorites(this.service)
+    new useCases.userfavoritesAndContacts.GetFavoritesAndContactedPropeties(this.service)
       .execute(user.id)
       .then((favorites) => res.status(200).json(favorites))
       .catch((error) => HandleError.handle(error, res));
@@ -20,18 +20,18 @@ export class FavoriteController {
   addFavorite = (req: Request, res: Response) => {
     const { property } = req.params;
     const { user } = req.body;
-    const [error, createDto] = CreateFavoriteDto.create({ property, user: user.id });
+    const [error, createFavoriteDto] = CreateFavoriteContactAdvDto.create({ property, user: user.id, favorite: true });
     if (error) return HandleError.handle(error, res);
 
-    new useCases.userfavorites.AddFavorite(this.service)
-      .execute(createDto!)
+    new useCases.userfavoritesAndContacts.AddFavorite(this.service)
+      .execute(createFavoriteDto!)
       .then((favorite) => res.status(200).json(favorite))
       .catch((error) => HandleError.handle(error, res));
   };
 
   removeFavorite = (req: Request, res: Response) => {
     const { id } = req.params;
-    new useCases.userfavorites.RemoveFavorite(this.service)
+    new useCases.userfavoritesAndContacts.RemoveFavorite(this.service)
       .execute(id)
       .then((favorite) => res.status(204).json(favorite))
       .catch((error) => HandleError.handle(error, res));
--- src/presentation/controllers/favorite/routes.ts
+++ src/presentation/controllers/favorite/routes.ts
@@ -1,25 +1,25 @@
 import { Router } from 'express';
 
 import { FavoriteController } from './controller';
-import { FavoriteServices } from '@presentation/service';
-import { FavoriteRepositoryImpl } from '@infrastructure/repository';
-import { FavoriteDatasourceImpl } from '@infrastructure/datasource';
-import { Favorite } from '@data/mongo/models';
+import { FavoriteContactServices } from '@presentation/service';
+import { FavoriteContactRepositoryImpl } from '@infrastructure/repository';
+import { FavoriteContactDatasourceImpl } from '@infrastructure/datasource';
+import { FavoriteAndContacted } from '@data/mongo/models';
 import { AuthMiddleware } from '@presentation/middlewares';
 
 export class FavoriteRoutes {
   static get routes(): Router {
     const router = Router();
 
-    const datasource = new FavoriteDatasourceImpl(Favorite);
-    const repository = new FavoriteRepositoryImpl(datasource);
-    const service = new FavoriteServices(repository);
+    const datasource = new FavoriteContactDatasourceImpl(FavoriteAndContacted);
+    const repository = new FavoriteContactRepositoryImpl(datasource);
+    const service = new FavoriteContactServices(repository);
     const controller = new FavoriteController(service);
 
     router.use(AuthMiddleware.authenticate);
 
-    router.get('/', controller.getUserFavorites);
-    router.post('/:property', controller.addFavorite);
+    router.get('/', controller.getFavoritesContactedProperties);
+    router.patch('/:property', controller.addFavorite);
     router.delete('/:id', controller.removeFavorite);
 
     return router;
