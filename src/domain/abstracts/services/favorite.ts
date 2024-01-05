85e4d9e83e1668692d859f2e0f86f8bec3a91eb2
Paul Llanque <llanquepol@gmail.com>
2024-01-05 15:26:46 -0500

    [DELETE] FavoriteService

--- src/domain/abstracts/services/favorite.ts
+++
@@ -1,8 +0,0 @@
-import { CreateFavoriteDto } from '@domain/dtos';
-import { FavoriteEntity } from '@domain/entities';
-
-export abstract class FavoriteService {
-  abstract getUserFavorites(user: string): Promise<FavoriteEntity[]>;
-  abstract addFavorite(dto: CreateFavoriteDto): Promise<FavoriteEntity>;
-  abstract removeFavorite(id: string): Promise<FavoriteEntity>;
-}
