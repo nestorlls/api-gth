e1175d729cec1fd6fa360d29cb4009be7b8b9675
Paul Llanque <llanquepol@gmail.com>
2024-01-04 20:33:30 -0500

    [UPDATE] remove update favorite method on abstract class

--- src/domain/abstracts/datasource/favorite.ts
+++ src/domain/abstracts/datasource/favorite.ts
@@ -1,10 +1,8 @@
-import { CreateFavoriteDto, UpdateFavoriteDto } from '@domain/dtos';
+import { CreateFavoriteDto } from '@domain/dtos';
 import { FavoriteEntity } from '@domain/entities';
 
 export abstract class FavoriteDatasource {
   abstract getUserFavorites(userId: string): Promise<FavoriteEntity[]>;
-  abstract getFavoriteById(id: string): Promise<FavoriteEntity>;
   abstract addFavorite(dto: CreateFavoriteDto): Promise<FavoriteEntity>;
-  abstract updateFavorite(dto: UpdateFavoriteDto): Promise<FavoriteEntity>;
   abstract removeFavorite(id: string): Promise<FavoriteEntity>;
 }
--- src/domain/abstracts/repository/favorite.ts
+++ src/domain/abstracts/repository/favorite.ts
@@ -1,9 +1,8 @@
-import { CreateFavoriteDto, UpdateFavoriteDto } from '@domain/dtos';
+import { CreateFavoriteDto } from '@domain/dtos';
 import { FavoriteEntity } from '@domain/entities';
 
 export abstract class FavoriteRepository {
   abstract getUserFavorites(userId: string): Promise<FavoriteEntity[]>;
   abstract addFavorite(dto: CreateFavoriteDto): Promise<FavoriteEntity>;
-  abstract updateFavorite(dto: UpdateFavoriteDto): Promise<FavoriteEntity>;
   abstract removeFavorite(id: string): Promise<FavoriteEntity>;
 }
