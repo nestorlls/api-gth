25e28396c8a86c76e28bee6d8ad1b091771ef18c
Paul Llanque <llanquepol@gmail.com>
2024-01-05 15:22:14 -0500

    [UPDATE] added fields favorite and contacted

--- src/domain/dtos/favorite/create.dto.ts
+++ src/domain/dtos/favorite/create.dto.ts
@@ -1,20 +1,22 @@
 import { Validator } from '@config/helpers';
 import { CustomeError } from '@domain/errors';
 
-export class CreateFavoriteDto {
+export class CreateFavoriteContactAdvDto {
   private constructor(
     public readonly user: string,
     public readonly property: string,
+    public readonly favorite?: boolean,
+    public readonly contacted?: boolean,
   ) {}
 
-  static create(props: CreateFavoriteDto): [CustomeError?, CreateFavoriteDto?] {
-    const { user, property } = props;
+  static create(props: CreateFavoriteContactAdvDto): [CustomeError?, CreateFavoriteContactAdvDto?] {
+    const { user, property, favorite = false, contacted = false } = props;
 
     if (!user) return [CustomeError.badRequest('User Id is required to create favorite')];
     if (!Validator.isMongoId(user)) return [CustomeError.badRequest('Invalid user id to create favorite')];
     if (!property) return [CustomeError.badRequest('Property Id is required to add favorite')];
     if (!Validator.isMongoId(property)) return [CustomeError.badRequest('Invalid property id to add favorite')];
 
-    return [undefined, new CreateFavoriteDto(user, property)];
+    return [undefined, new CreateFavoriteContactAdvDto(user, property, favorite, contacted)];
   }
 }
