72704653ee7d743f7257aa7c279c28c994b63127
Paul Llanque <llanquepol@gmail.com>
2024-01-05 15:25:38 -0500

    [DELETE] favori abstract repository

--- src/domain/abstracts/repository/favorite.ts
+++
@@ -1,8 +0,0 @@
-import { CreateFavoriteDto } from '@domain/dtos';
-import { FavoriteEntity } from '@domain/entities';
-
-export abstract class FavoriteRepository {
-  abstract getUserFavorites(userId: string): Promise<FavoriteEntity[]>;
-  abstract addFavorite(dto: CreateFavoriteDto): Promise<FavoriteEntity>;
-  abstract removeFavorite(id: string): Promise<FavoriteEntity>;
-}
