554ed08877cebc6c75a6354d08f4b7dfde7a6c52
Paul Llanque <llanquepol@gmail.com>
2024-01-23 17:25:23 -0500

    [REFACTOR] upload middleware

--- src/presentation/middlewares/upload.middleware.ts
+++ src/presentation/middlewares/upload.middleware.ts
@@ -6,46 +6,69 @@ import { NextFunction, Request, Response } from 'express';
 import { Uuid } from '@config/adapters';
 
 export class UploadMiddleware {
-  static uploadFile(req: Request, res: Response, next: NextFunction) {
-    const type = req.url.split('/').at(-1);
-    const pathFile = path.resolve(__dirname, '../../..', `uploads/${type}`);
+  static uploadFile = (req: Request, res: Response, next: NextFunction) => {
+    const type = req.url.split('/').at(-1) as string;
+    const filePath = path.resolve(__dirname, '../../../uploads', type);
+
+    const isPathExists = fs.existsSync(filePath);
+    if (!isPathExists) {
+      fs.mkdirSync(filePath, { recursive: true });
+    }
 
     const storage = multer.diskStorage({
-      destination: function (req, file, callback) {
-        callback(null, pathFile);
+      destination: function (req, file, cb) {
+        cb(null, filePath);
       },
-      filename: function (req, file, callback) {
-        const ext = file.originalname.split('.').pop();
-        const fileName = `${Uuid.v4()}.${ext}`;
-        callback(null, fileName);
+      filename: function (req, file, cb) {
+        const extension = path.extname(file.originalname);
+        const filename = `${Uuid.v4()}${extension}`;
+        cb(null, filename);
       },
     });
 
     const upload = multer({
       storage,
       limits: {
-        fileSize: 5 * 1024 * 1024,
+        fileSize: 1024 * 1024 * 5,
+      },
+      fileFilter: (req, file, cb) => {
+        const validExtensions = ['.png', '.jpg', '.jpeg'];
+        const ext = path.extname(file.originalname);
+        if (!validExtensions.includes(path.extname(file.originalname))) {
+          return cb(new Error(`Invalid file type '${ext}'. Valid types: ${validExtensions.join(', ')}`));
+        }
+        cb(null, true);
       },
     });
 
-    const exists = fs.existsSync(pathFile);
-    if (!exists) {
-      fs.mkdirSync(pathFile);
-    }
-
-    upload.array('file')(req, res, (err) => {
+    upload.array('files', 5)(req, res, (err) => {
       if (err) {
-        return res.status(400).json({
-          error: err.message,
+        return res.status(400).send({
+          status: 400,
+          message: err.message,
         });
       }
 
       if (!req.files) {
-        return res.status(400).json({
-          error: 'No files were uploaded.',
+        return res.status(400).send({
+          status: 400,
+          message: 'No file uploaded',
         });
       }
+      req.body.files = req.files;
+      req.body.type = type;
+
       next();
     });
-  }
+  };
+
+  static validateType = (req: Request, res: Response, next: NextFunction) => {
+    const type = req.url.split('/').pop() as string;
+    const validTypes = ['property', 'user'];
+
+    if (!validTypes.includes(type)) {
+      return res.status(400).json({ message: `Invalid type '${type}'. Valid types: ${validTypes.join(', ')}` });
+    }
+    next();
+  };
 }
