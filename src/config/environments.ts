ad5bbfa3b0c5fc31b6736cfeebcce3914276bf29
Paul Llanque <llanquepol@gmail.com>
2024-01-22 19:10:23 -0500

    [ADD] cloudinary envs

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -8,4 +8,7 @@ export const envs = {
   MONGO_DB_URL: get('MONGO_DB_URL').required().asString(),
   MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
   JWT_SECRET: get('JWT_SECRET').required().asString(),
+  CLOUD_NAME: get('CLOUD_NAME').required().asString(),
+  API_KEY: get('API_KEY').required().asString(),
+  API_SECRET: get('API_SECRET').required().asString(),
 };
