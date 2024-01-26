20cbdd477a455e66e10b0a470de53a8715aced5d
Paul Llanque <llanquepol@gmail.com>
2024-01-26 18:10:16 -0500

    [UPDATE] upload file dto adding type File

--- src/domain/dtos/upload/uploadFile.dto.ts
+++ src/domain/dtos/upload/uploadFile.dto.ts
@@ -1,8 +1,19 @@
 import { CustomeError } from '@domain/errors';
 
+type File = {
+  path: string;
+  originalname: string;
+  fieldname: string;
+  encoding: string;
+  mimetype: string;
+  destination: string;
+  filename: string;
+  size: number;
+};
+
 export class UploadFileDto {
   private constructor(
-    public readonly files: Express.Multer.File[],
+    public readonly files: File[],
     public readonly type: string,
   ) {}
 
