b8f0a3a50508c00b4fa94389d5cd70a415ab5469
Paul Llanque <llanquepol@gmail.com>
2024-01-26 18:15:00 -0500

    [UPDATE] adding paths

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,14 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@data/(.*)$': '<rootDir>/src/data/$1',
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '^@presentation/(.*)$': '<rootDir>/src/presentation/$1',
+  },
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
