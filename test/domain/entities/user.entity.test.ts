d13064fa962c6bd51164dd2fb8ccfae3c21b6df4
Paul Llanque <llanquepol@gmail.com>
2024-02-03 13:13:19 -0500

    [ADD] test user entity

---
+++ test/domain/entities/user.entity.test.ts
@@ -0,0 +1,50 @@
+import { UserEntity } from '../../../src/domain/entities';
+import { CustomeError } from '../../../src/domain/errors';
+
+describe('Test User Entity', () => {
+  test('should create a new UserEntity with all required properties', () => {
+    const props = {
+      id: '507f191e810c19729de860ea',
+      name: 'test',
+      email: 'test@test.com',
+      phone: 999999999,
+      role: ['homeseeker'],
+      password: 'Password1',
+      avatar: '',
+    };
+
+    const user = new UserEntity(props);
+
+    expect(user).toBeInstanceOf(UserEntity);
+    expect(user).toHaveProperty('id', props.id);
+    expect(user).toHaveProperty('name', props.name);
+    expect(user).toHaveProperty('email', props.email);
+  });
+
+  test('should throw a bad request if passing an invalid id', () => {
+    const props = {
+      id: '1234',
+      name: 'test',
+      email: 'test@test.com',
+      phone: 999999999,
+      role: ['homeseeker'],
+      password: 'Password1',
+      avatar: '',
+    };
+
+    expect(() => UserEntity.fromObject(props)).toThrow(CustomeError.badRequest('Invalid user id'));
+  });
+
+  test('should throw a bad request if passing an invalid name', () => {
+    const props = {
+      id: '507f191e810c19729de860ea',
+      email: 'test@test.com',
+      phone: 999999999,
+      role: ['homeseeker'],
+      password: 'Password1',
+      avatar: '',
+    };
+
+    expect(() => UserEntity.fromObject(props)).toThrow(CustomeError.badRequest('Missing name'));
+  });
+});
