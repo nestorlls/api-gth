4250b0b9abcc527a400634aee338e4fca28f821b
Paul Llanque <llanquepol@gmail.com>
2024-02-03 20:30:29 -0500

    [UPDATE] map from object entity

--- test/domain/entities/user.entity.test.ts
+++ test/domain/entities/user.entity.test.ts
@@ -21,6 +21,24 @@ describe('Test User Entity', () => {
     expect(user).toHaveProperty('email', props.email);
   });
 
+  test('should map an object to a UserEntity', () => {
+    const props = {
+      id: '507f191e810c19729de860ea',
+      name: 'test',
+      email: 'test@test.com',
+      phone: 999999999,
+      role: ['homeseeker'],
+      password: 'Password1',
+      avatar: '',
+    };
+
+    const user = UserEntity.fromObject(props);
+
+    expect(user).toBeInstanceOf(UserEntity);
+    expect(user).toHaveProperty('id', props.id);
+    expect(user).toHaveProperty('name', props.name);
+  });
+
   test('should throw a bad request if passing an invalid id', () => {
     const props = {
       id: '1234',
