9904316911af67eaff798ebd1aa68ad84b16698d
Paul Llanque <llanquepol@gmail.com>
2024-02-03 20:35:47 -0500

    [ADD] test register use case

---
+++ test/domain/user-cases/register.user-case.test.ts
@@ -0,0 +1,19 @@
+import { Register } from '../../../src/domain/use-cases/auth';
+import { RegisterDto } from '../../../src/domain/dtos/auth';
+
+describe('Test Register User Case', () => {
+  const mockAuthService = {
+    login: jest.fn().mockReturnValue({ user: {}, token: 'token' }),
+    register: jest.fn().mockReturnValue({ user: {}, token: 'token' }),
+  };
+
+  test('should call Register Use case', async () => {
+    const register = new Register(mockAuthService);
+    const registerDto = RegisterDto.create({ email: 'j@j.com', password: '123' })[1];
+
+    const result = await register.execute(registerDto!);
+
+    expect(result).toEqual({ token: 'token', user: {} });
+    expect(mockAuthService.register).toHaveBeenCalledWith(registerDto);
+  });
+});
