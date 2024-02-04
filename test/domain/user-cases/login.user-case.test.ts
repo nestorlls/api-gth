1e29c19f65bb89b5c6fabba4ff6ba9dd2896b945
Paul Llanque <llanquepol@gmail.com>
2024-02-03 20:35:35 -0500

    [ADD] test login use case

---
+++ test/domain/user-cases/login.user-case.test.ts
@@ -0,0 +1,19 @@
+import { Login } from '../../../src/domain/use-cases/auth';
+import { LoginDto } from '../../../src/domain/dtos/auth';
+
+describe('Test Register User Case', () => {
+  const mockAuthService = {
+    login: jest.fn().mockReturnValue({ user: {}, token: 'token' }),
+    register: jest.fn().mockReturnValue({ user: {}, token: 'token' }),
+  };
+
+  test('should call Login Use case', async () => {
+    const login = new Login(mockAuthService);
+    const loginDto = LoginDto.create({ email: 'j@j.com', password: '123' })[1];
+
+    const result = await login.execute(loginDto!);
+
+    expect(result).toEqual({ token: 'token', user: {} });
+    expect(mockAuthService.login).toHaveBeenCalledWith(loginDto);
+  });
+});
