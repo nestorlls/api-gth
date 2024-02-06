30dbf89d10e95634780e8f0081703355088085a8
Paul Llanque <llanquepol@gmail.com>
2024-02-06 18:55:52 -0500

    [UPDATE] testing if trhow an custome error

--- test/intrastructure/datasource/auth.datasource.test.ts
+++ test/intrastructure/datasource/auth.datasource.test.ts
@@ -4,6 +4,7 @@ import { envs } from '../../../src/config/environments';
 import { MongoDB } from '../../../src/data/mongo/connection/mongo.connection';
 import { AuthDatasourceImpl } from '../../../src/infrastructure/datasource';
 import { RegisterDto } from '../../../src/domain/dtos';
+import { CustomeError } from '../../../src/domain/errors';
 
 describe('Test Authentication Datasource', () => {
   beforeAll(async () => {
@@ -36,6 +37,24 @@ describe('Test Authentication Datasource', () => {
     expect(mockAuthDatasource.register).toHaveBeenCalledWith(registerDto);
   });
 
+  test('should throw an error if user already exists', async () => {
+    const user = {
+      name: 'Paul',
+      email: 'j@j.com',
+      password: 'Passowrd1',
+      phone: '123',
+      role: 'homeseeker',
+    };
+
+    try {
+      await mockAuthDatasource.register(user);
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(error).toBeInstanceOf(CustomeError);
+      expect(`${error}`).toEqual('Bad Request: User already exists');
+    }
+  });
+
   test('should login a user', async () => {
     const loginSpy = jest.spyOn(mockAuthDatasource, 'login');
     const user = {
@@ -50,5 +69,20 @@ describe('Test Authentication Datasource', () => {
     expect(mockAuthDatasource.login).toHaveBeenCalledTimes(1);
 
     await User.deleteMany({});
+  });
+
+  test('should throw an error if user not found', async () => {
+    const user = {
+      email: 'j@j.com',
+      password: 'Passowrd1',
+    };
+
+    try {
+      await mockAuthDatasource.login(user);
+      expect(true).toBe(false);
+    } catch (error) {
+      expect(error).toBeInstanceOf(CustomeError);
+      expect(`${error}`).toEqual('Not Found: User not found');
+    }
   });
 });
